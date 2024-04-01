dbdf6f0a3f3f0024ad4917e4c93024a9b6273f9a
mudassar864 <mohsanbhatti864@gmail.com>
2024-04-02 00:02:19 +0500

    contact

--- app/(root)/contact-us/page.jsx
+++ app/(root)/contact-us/page.jsx
@@ -14,11 +14,12 @@ export default function page() {
           Get In Touch
         </h1>
         <div className="font-benton text-md md:text-xl text-center font-light md:w-3/5 mx-auto p-4">
-          For those who demand an elevated service like none other, there's
-          Dubai Sotheby's International Realty. We're the industry's best
-          agents, curating with incomparable attention to style and detail.
-          We're here for you, to help you buy or sell your home at a scale you
-          just won't find anywhere else.
+          Experience unparalleled service and attention to detail with the
+          assistance of an award-winning Global consultant and Associate
+          Director at Dubai Sotheby's International Realty. With a track record
+          of facilitating VIPs worldwide in their endeavors to live and invest
+          in the world's most prestigious destinations, let me elevate your real
+          estate experience to new heights. Get in touch today.
         </div>
         <div className="block lg:hidden" style={{ height: 15 }} />
         <div className="hidden lg:block" style={{ height: 30 }} />
@@ -26,14 +27,14 @@ export default function page() {
           <div className="wide-container !max-w-[1518px]  text-white border-0 flex flex-col lg:flex-row mx-auto">
             <div className="w-full lg:w-1/2 pt-12 pb-[46px] lg:pb-[72px] lg:pl-5 xl:pl-[180px] font-normal flex justify-center lg:justify-start">
               <div className="container !max-w-[461px] xl:!ml-0">
-                <div className="mb-6 text-2xl">Get in touch</div>
+                <div className="mb-6 text-2xl">Get in Touch</div>
                 <div className="mb-5 font-benton text-[15px] leading-[22px] tracking-[0.375px]">
                   Please complete the form below, and your message will be
                   promptly delivered to the appropriate department and staff
                   member who will contact you in the next 24 hours. <br />
                   <br />
                   If you require immediate assistance, please don't hesitate to
-                  call or WhatsApp on +971 527 180606.
+                  call or WhatsApp on <strong>+971 527 180606</strong>.
                 </div>
                 <AddForm id="no" />
               </div>
@@ -43,7 +44,12 @@ export default function page() {
                 <div className="mb-5 lg:mb-6 text-2xl ">Office</div>
                 <div className="font-benton sm:grid  md:py-5">
                   <div className=" w-full">
-                    <Image src="/assets/logo_ae.svg" width={300} height={400} alt="sothebys logo" />
+                    <Image
+                      src="/assets/logo_ae.svg"
+                      width={300}
+                      height={400}
+                      alt="sothebys logo"
+                    />
                     <p className="tracking-[0.375px] text-[15px] leading-[22px] mb-0 mt-4">
                       EMAAR Business Park,{" "}
                     </p>
