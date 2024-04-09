147064a4b2b2ddf076656a2e35872714496d6b09
mudassar864 <mohsanbhatti864@gmail.com>
2024-04-09 15:48:33 +0500

    checking video

--- app/api/video/route.js
+++ app/api/video/route.js
@@ -2,31 +2,38 @@ import fs from "fs";
 import path from "path";
 
 function getVideoStream(req) {
-  const range = req.headers.get("range");
-  if (!range) {
-    return new Response("Ruquires Range Header", { status: 400 });
+  try {
+    const range = req.headers.get("range");
+    if (!range) {
+      return new Response("Requires Range Header", { status: 400 });
+    }
+    const filePath = "./public/assets/intro.mp4"; // Replace with your actual file path
+
+    const videoSize = fs.statSync(filePath).size;
+    const CHUNK_SIZE = 10 ** 6; // 1 MB
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+    const contentLength = end - start + 1;
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": contentLength.toString(),
+      "Content-Type": "video/mp4",
+      "Access-Control-Allow-Origin": "*", // Replace with your domain
+    };
+    const videoStram = fs.createReadStream(filePath, {
+      start,
+      end,
+    });
+
+    return new Response(videoStram, { status: 206, headers });
+  } catch (error) {
+    // If an error occurs, handle it and return an appropriate response
+    console.error("Error:", error);
+    return new Response("Internal Server Error", { status: 500 });
   }
-  const filePath = "./public/assets/intro.mp4"; // Replace with your actual file path
-
-  const videoSize = fs.statSync(filePath).size;
-  const CHUNK_SIZE = 10 ** 6; //1 MB
-  const start = Number(range.replace(/\D/g, ""));
-  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-  const contentLength = end - start + 1;
-  const headers = {
-    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-    "Accept-Ranges": "bytes",
-    "Content-Length": contentLength.toString(),
-    "Content-Type": "video/mp4",
-    "Access-Control-Allow-Origin": "*", // Replace with your domain
-  };
-  const videoStram = fs.createReadStream(filePath, {
-    start,
-    end,
-  });
-
-  return new Response(videoStram, { status: 206, headers });
 }
+
 export async function GET(req) {
   return getVideoStream(req);
 }
