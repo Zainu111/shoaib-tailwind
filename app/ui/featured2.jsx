f3e13b1d4d881f8a9fb624881814407be2a02cc2
Mudassar Manzoor <mohsanbhatti864@gmail.com>
2025-03-03 17:18:23 +0500

    add

--- app/ui/featured2.jsx
+++ app/ui/featured2.jsx
@@ -6,7 +6,7 @@ import redseaicon from "@/public/assets/logo-hero.svg";
 import beach from "@/public/assets/Shura MASTER PLAN.jpg";
 export default function Featured2() {
   return (
-    <section className="w-full h-[80vh] relative">
+    <section className="w-full h-[35vh] md:h-[50vh] lg:h-[90vh] relative">
       <Image
         src={beach}
         className="w-full h-full object-fill"
